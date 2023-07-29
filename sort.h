e810ddfc03708d2b6487cf36d8f8ae5c79251f7e
anasjelloule <anasjelloule@gmail.com>
2023-07-29 04:36:27 -0700

    task shaker algo/1

--- sort.h
+++ sort.h
@@ -18,7 +18,7 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 void swtch_ints(int *a, int *b);
-void cocktail_sort_list(listint_t **list)
+void cocktail_sort_list(listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
